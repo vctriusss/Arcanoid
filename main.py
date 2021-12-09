4bcd8e29caba9ed97a130d42b55d9d01e84855ca
vctriusss <vskamaev@miem.hse.ru>
2021-12-09 22:06:25 +0300

    adding oop and platform resize

--- main.py
+++ main.py
@@ -1,37 +1,41 @@
 import pygame as pg
 
-res = screen_width, screen_height = 1200, 800
+res = screen_width, screen_height = 1200, 720
 fps = 60
 
-# class Platform:
-#     def __init__(self):
-#         self.width = 300
-#         self.height = 30
-#         self.speed = 10
-#         self.body = pg.Rect(screen_width // 2 - self.width // 2,
-#                             screen_height - self.height, self.width, self.height)
 
-platform_width, platform_height = 300, 30
-platform_speed = 15
-platform = pg.Rect(screen_width // 2 - platform_width // 2, screen_height - platform_height,
-                   platform_width, platform_height)
+class Platform:
+    def __init__(self):
+        self.width = 100
+        self.height = 10
+        self.speed = 10
+        self.body = pg.Rect(screen_width // 2 - self.width // 2, screen_height - self.height - 5,
+                            self.width, self.height)
+
+    def resize(self):
+        pg.draw.rect(screen, pg.Color('white'), platform.body)
+        self.body = self.body.inflate(15, 0)
+
+
+platform = Platform()
 
 pg.init()
 screen = pg.display.set_mode(res)
+img = pg.image.load('image.jpg').convert()
 clock = pg.time.Clock()
 
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit()
-    pg.draw.rect(screen, pg.Color('blue'), platform)
+    screen.blit(img, (0, 0))
+    pg.draw.rect(screen, pg.Color('magenta'), platform.body)
     key = pg.key.get_pressed()
-    if key[pg.K_a] and platform.left > 0:
-        platform.left -= platform_speed
+    if key[pg.K_a] and platform.body.left > 0:
+        platform.body.left -= platform.speed
 
-    if key[pg.K_d] and platform.right < screen_width:
-        platform.right += platform_speed
-    if key[pg.K_w]:
-        screen.blit(img, (0, 0))
+    if key[pg.K_d] and platform.body.right < screen_width:
+        platform.body.right += platform.speed
+
     pg.display.flip()
     clock.tick(fps)
