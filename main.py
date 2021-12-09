fcf06579ce3d5658a70d420d353659023bc77ad9
vctriusss <vskamaev@miem.hse.ru>
2021-12-09 20:36:24 +0300

    platform movement and requirements

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 import pygame as pg
 
-screen_width, screen_height = 1200, 800
+res = screen_width, screen_height = 1200, 800
 fps = 60
 
+# class Platform:
+#     def __init__(self):
+#         self.width = 300
+#         self.height = 30
+#         self.speed = 10
+#         self.body = pg.Rect(screen_width // 2 - self.width // 2,
+#                             screen_height - self.height, self.width, self.height)
 
-class Platform:
-    def __init__(self):
-        self.length = 300
-        self.height = 30
-        self.speed = 10
+platform_width, platform_height = 300, 30
+platform_speed = 15
+platform = pg.Rect(screen_width // 2 - platform_width // 2, screen_height - platform_height,
+                   platform_width, platform_height)
 
-    def resize(self, c):
-        self.length *= c
-
-
-platform = Platform()
 pg.init()
-screen = pg.display.set_mode((screen_width, screen_height))
+screen = pg.display.set_mode(res)
 clock = pg.time.Clock()
 
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit()
+    pg.draw.rect(screen, pg.Color('blue'), platform)
+    key = pg.key.get_pressed()
+    if key[pg.K_a] and platform.left > 0:
+        platform.left -= platform_speed
 
-
+    if key[pg.K_d] and platform.right < screen_width:
+        platform.right += platform_speed
+    if key[pg.K_w]:
+        screen.blit(img, (0, 0))
     pg.display.flip()
     clock.tick(fps)
