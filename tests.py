d4b8c9fa914a466788d99e173829f9f14350e14b
vctriusss <vskamaev@miem.hse.ru>
2021-12-18 11:17:21 +0300

    more tests

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import unittest
-from main import Ball, ObjectCollision
+from main import Ball, ObjectCollision, Platform, Bonus
 from Block import Block
-import pygame as pg
+from constants import *
 
 
 class GameTest(unittest.TestCase):
@@ -13,10 +13,33 @@ class GameTest(unittest.TestCase):
         ball = Ball(0, 0)
         self.assertFalse(ball.IsOut())
 
-    def right_collision(self):
+    def test_right_collision(self):
         ball = Ball(100, 100)
         block = Block(50, 50, 60, 60, pg.Color('white'))
         self.assertTrue(ObjectCollision(block, ball))
+
+    def test_correct_new_direction(self):
+        ball = Ball(600, screen_height - 17)
+        ball.dy = 1
+        platform = Platform()
+        ObjectCollision(platform, ball)
+        self.assertEqual(ball.dy, -1)
+
+    def test_wall_bounce(self):
+        ball = Ball(screen_width - 16, 100)
+        ball.dx = 1
+        ball.WallBounce()
+        self.assertEqual(ball.dx, -1)
+
+    def test_bonus_apply(self):
+        block = Block(600, 1, 1, 1, (0, 0, 0))
+        block.bonus = bonuses[0]
+        bonus = Bonus(block)
+        platform = Platform()
+        bonus.body.bottom = platform.body.top + 1
+        if ObjectCollision(platform, bonus):
+            exec(bonus.bonus)
+        self.assertEqual(platform.body.width, 187)
 
 
 if __name__ == '__main__':
